916c0dd022290d7052803ff3439251188cd996ca
Samuel Zaffran <zaffransamuel@gmail.com>
2024-09-11 18:22:29 +0300

    trying

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -52,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
         redirect: Redirect(
           native: 'web3modalflutter://',
-          // universal: 'https://walletconnect.com/appkit',
+          universal: 'https://walletconnect.com/appkit',
         ),
       );
 
@@ -179,22 +179,7 @@ class _MyHomePageState extends State<MyHomePage> {
         loginWithoutWalletWidget: null,
         siweConfig: _siweConfig(siweAuthValue),
         enableAnalytics: analyticsValue, // OPTIONAL - null by default
-        enableEmail: false, // OPTIONAL - false by default
-        // requiredNamespaces: {},
-        // optionalNamespaces: {},
-        // includedWalletIds: {},
-        /*featuredWalletIds: {
-          'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase
-          '18450873727504ae9315a084fa7624b5297d2fe5880f0982979c17345a138277', // Kraken Wallet
-          'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // Metamask
-          '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369', // Rainbow
-          'c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a', // Uniswap
-          '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662', // Bitget
-        },*/
-        // excludedWalletIds: {
-        //   'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase
-        // },
-        // MORE WALLETS https://explorer.walletconnect.com/?type=wallet&chains=eip155%3A1
+        enableEmail: emailWalletValue, // OPTIONAL - false by default
       );
       setState(() => _initialized = true);
     } on W3MServiceException catch (e) {
